9452146d2ba85d9165a3f81f43d7e96b4e8eed4f
Victor Reyes <vmreyes@mit.edu>
2017-07-22 11:40:09 -0700

    setup dummy website

--- main.py
+++ main.py
@@ -14,14 +14,13 @@
 
 import webapp2
 import jinja2
-env = Environment(
-    loader=PackageLoader('templates')
-)
+
+env=jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-
+        template = env.get_template('index.html')
         self.response.write(template.render())
 
 
