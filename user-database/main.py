df58cf74c0e012cb9c0d6e5c544ba9c61ee0dde6
Demo User <demouser@MacBook-Air.local>
2017-07-24 15:53:08 -0700

    database

--- user-database/main.py
+++ user-database/main.py
@@ -1,23 +1,27 @@
 from users import UserStorage
 import jinja2
-import os
 import webapp2
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(_file_)))
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('user_templates'))
 
-
-class MainHandler(webapp2.RequestHandler):
+class UserHandler(webapp2.RequestHandler):
     def get(self):
-        UserStorage(id = "Prado Inciong", email="prado_jix@yahoo.com", LatLocation = 33.99, LongLocation= -118.47  ).put()
-        UserStorage(id = "Jasmine Chau", email="Jasmine_Chau@yahoo.com", LatLocation = 55.0, LongLocation= 118.47  ).put()
-        UserStorage(id = "Victor Reyes", email="Victor_Reyes@yahoo.com", LatLocation = -3.99, LongLocation= -118.47  ).put()
+        main_template = env.get_template('user.html')
+        self.response.out.write(main_template.render())
+
+    def post(self):
+        user_name = self.request.get("user_name")
+        user_email = self.request.get("email")
+        user_LatLocation = float(self.request.get("user_LatLocation"))
+        user_LongLocation = float(self.request.get("user_LongLocation"))
+        UserStorage(id = user_name, email=user_email, LatLocation = user_LatLocation, LongLocation= user_LongLocation ).put()
+
         user_query = UserStorage.query()
         users = user_query.fetch()
-        self.response.write(users)
+        #self.response.write(users)
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', UserHandler)
 ], debug=True)
